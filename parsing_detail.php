eb324fc0746dde08763141cc5f347544901440a5
Dwi Setia Fardhana <1931110044@student.polinema.ac.id>
2021-08-28 18:51:45 +0700

    CARA PARSING-DETAIL SETIAL ITEM

---
+++ parsing_detail.php
@@ -0,0 +1,23 @@
+<?php
+
+$data = "1_OK#2_NG#3_OK#4_NG#5_OK#6_OK#7_NG#8_NG#9_NG#10_Ok";
+
+$newData = explode('#', $data);
+$stateBotol = [];
+
+// PUSH
+for ($i = 0; $i < count($newData); $i++) {
+    $bufferNewData = explode('_', $newData[$i]); // PER ITEM
+    array_push($stateBotol, $bufferNewData[1]);
+}
+
+// PRINT ARRAY
+print_r($stateBotol);
+echo "<br>";
+
+for ($i = 1; $i <= count($stateBotol); $i++) {
+    echo "STATUS BOTOL ".$i." : ".$stateBotol[$i-1];
+    echo "<br>";
+}
+
+?>
